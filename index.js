71065354598152614b0c24a7f35bdf4d53de34a9
bluehostt <44468166+bluehostt@users.noreply.github.com>
2019-01-20 12:41:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('-bc')){
-if(!message.author.id === '476185102922285066') return;
+if(!message.author.id === '333140974865874944') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
